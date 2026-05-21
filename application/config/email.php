399af20cd7047fbc59b1a49dce834274e1de5874
Zayyan0310 <zayyandzul@gmail.com>
2026-05-22 06:27:40 +0700

    remove smtp from email.php

--- application/config/email.php
+++ application/config/email.php
@@ -5,14 +5,14 @@ return array(
 	'protocol'    => 'smtp',
 	'smtp_host'   => 'smtp.gmail.com',
 	'smtp_port'   => 465,
-	'smtp_user'   => 'blckclover9@gmail.com',
-	'smtp_pass'   => 'vdskxistkkjgzxqy', // App Password
+	'smtp_user'   => '', #email yang digunakan untuk mengirim email
+	'smtp_pass'   => '', #app password untuk email
 	'smtp_crypto' => 'ssl',
 	'mailtype'    => 'html',
 	'charset'     => 'utf-8',
 	'newline'     => "\r\n",
 
 	// ✅ HARUS SAMA DENGAN smtp_user
-	'from_email'  => 'blckclover9@gmail.com',
-	'from_name'   => 'HRD Perusahaan'
+	'from_email'  => '', #email yang digunakan sebagai pengirim
+	'from_name'   => 'Admin' #nama yang ditampilkan sebagai pengirim
 );
